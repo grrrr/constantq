47d77cf2a722c5066d710409ab1c1e4d483e6484
Thomas Grill <gr@grrrr.org>
2008-07-12 21:57:05 +0000

    added sms externals updates for som and ntree
    git-svn-id: https://svn.grrrr.org/ext/trunk/constantq@3618 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ class constantq
 public:
     constantq(const t_symbol *bname)
         : cq(NULL)
-        , threshold(0.01),minhop(64)
+        , threshold(0.001),minhop(64),wndalign(1),srate(44100)
     {
         buf.Set(bname);
         
@@ -35,6 +35,8 @@ protected:
         FLEXT_CADDATTR_VAR1(c,"threshold",threshold);
         FLEXT_CADDATTR_VAR1(c,"minhop",minhop);
         FLEXT_CADDATTR_GET(c,"length",mg_length);
+        FLEXT_CADDATTR_VAR(c,"wndalign",wndalign,ms_wndalign);
+        FLEXT_CADDATTR_VAR(c,"srate",srate,ms_srate);
     }
     
     // transform buffer to list
@@ -44,7 +46,8 @@ protected:
             blitz::Array<t_sample,1> data(buf.Data(),shape(buf.Frames()),neverDeleteData);
             blitz::Array<t_sample,1> ref(data,Range(0,cq->length()-1));
             blitz::Array<complex<t_sample>,1> tr = (*cq)(ref);
-            AtomList ret(tr.length(0));
+            
+            AtomListStatic<64> ret(tr.length(0));
             for(int i = 0; i < ret.Count(); ++i)
                 SetFloat(ret[i],abs(tr(i)));
             ToOutList(0,ret);
@@ -58,8 +61,7 @@ protected:
         for(int i = 0; i < argc; ++i)
             freqs(i) = GetAFloat(argv[i]);
             
-        delete cq;
-        cq = new ConstantQ<t_sample>(44100,freqs,Window::Window<Window::Hamming<t_sample> >(),threshold,0,minhop);
+        refreshkernel();
     }
 
     void ms_freqs(const AtomList &l) { m_freqs(l.Count(),l.Atoms()); }
@@ -76,19 +78,50 @@ protected:
         l = cq?cq->length():0;
     }
     
+    void ms_wndalign(int wa)
+    {
+        if(wndalign != wa) {
+            wndalign = wa;
+            refreshkernel();
+        }
+    }
+    
+    void ms_srate(float sr)
+    {
+        if(srate != sr) {
+            srate = sr;
+            refreshkernel();
+        }
+    }
+    
     buffer buf;
     blitz::Array<t_sample,1> freqs;
     float threshold;
-    int minhop;
+    int minhop,wndalign;
+    float srate;
     ConstantQ<t_sample> *cq;
     
 private:
+
+    void refreshkernel()
+    {
+        delete cq;
+        if(freqs.length(0))
+            cq = new ConstantQ<t_sample>(srate,freqs,Window::Window<Window::Hamming<t_sample> >(),threshold,wndalign,minhop);
+        else
+            cq = NULL;
+    }
+
     FLEXT_CALLBACK(m_bang)
     FLEXT_CALLBACK_V(m_freqs)
     FLEXT_CALLVAR_V(mg_freqs,ms_freqs)
     FLEXT_ATTRVAR_F(threshold)
     FLEXT_ATTRVAR_F(minhop)
     FLEXT_CALLGET_I(mg_length)
+    FLEXT_ATTRGET_I(wndalign)
+    FLEXT_CALLSET_I(ms_wndalign)
+    FLEXT_ATTRGET_F(srate)
+    FLEXT_CALLSET_F(ms_srate)
 };
 
 FLEXT_NEW_1("constantq",constantq,t_symptr)
